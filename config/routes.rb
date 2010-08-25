67c3473930de92dd9bf803e1406ac98076dd6d9c
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-24 22:44:10 -0700

    Setup user_session routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :user_session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -32,6 +34,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => "user_sessions", :action => "new"
 
   # See how all your routes lay out with "rake routes"
 
