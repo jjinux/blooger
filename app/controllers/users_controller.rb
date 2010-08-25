33b76d916de40ec8b5a9bd600e750d180df56c45
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-24 23:04:01 -0700

    Updated UsersController to make sense.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  before_filter :require_no_user, :only => [:new, :create]
+  before_filter :require_user,    :only => [:show, :edit, :update]
+  before_filter :find_user,       :only => [:show, :edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success_message] = "Account registered!"
+      redirect_to root_url
+    else
+      render :action => :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success_message] = "Account updated!"
+      redirect_to(@user)
+    else
+      render :action => :edit
+    end
+  end
+
+  private
+
+  def find_user
+    @user = @current_user
+  end
 end
