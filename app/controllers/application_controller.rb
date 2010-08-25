c326ca8c213b984af12b16b90eba35d3a0321877
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-25 11:24:41 -0700

    Tweaked flash messages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     unless current_user
-      flash[:error_message] = "You must be logged in to access this page"
+      flash[:notice] = "You must be logged in to access this page"
       redirect_to new_user_session_url
       return false
     end
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
 
   def require_no_user
     if current_user
-      flash[:error_message] = "You must be logged out to access this page"
+      flash[:notice] = "You must be logged out to access this page"
       redirect_to current_user
       return false
     end
