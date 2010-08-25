a92910a2ffc9ea0aedcdc01094f84d80c45fd1cc
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-24 23:03:33 -0700

    Added a permissions_denied method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,8 @@ class ApplicationController < ActionController::Base
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.user
   end
+
+  def permission_denied
+    render :text => "Forbidden", :status => "403 Forbidden"
+  end
 end
