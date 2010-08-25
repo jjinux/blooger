ddea3bca19ca8d634cc8c48d45f6e0230abac9e2
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-24 21:41:50 -0700

    Added fields for Authlogic.

--- db/migrate/20100825043540_create_users.rb
+++ db/migrate/20100825043540_create_users.rb
@@ -1,6 +1,22 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
+      t.string    :username,            :null => false
+      t.string    :crypted_password,    :null => false
+      t.string    :password_salt,       :null => false
+      t.string    :blog_title,          :null => false
+      t.string    :persistence_token,   :null => false
+      t.string    :single_access_token, :null => false
+      t.string    :perishable_token,    :null => false
+
+      # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
+      t.integer   :login_count,         :null => false, :default => 0
+      t.integer   :failed_login_count,  :null => false, :default => 0
+      t.datetime  :last_request_at
+      t.datetime  :current_login_at
+      t.datetime  :last_login_at
+      t.string    :current_login_ip
+      t.string    :last_login_ip
 
       t.timestamps
     end
