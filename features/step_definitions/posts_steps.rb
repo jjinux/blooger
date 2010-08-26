21875d535895b8e1da07c932cb242289f7435a7d
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-26 02:08:34 -0700

    Test that Markdown is being used.

--- features/step_definitions/posts_steps.rb
+++ features/step_definitions/posts_steps.rb
@@ -1,3 +1,7 @@
 Given /^there is only one post$/ do
   @user.posts.create!(:title => "One Post Title", :body => "One post body.")
 end
+
+Then /^I should see Markdown in strong tags$/ do
+  response.should have_selector("strong", :content => "Markdown")
+end
