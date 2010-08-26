faae1505c8d20ac18df49739291191a3ad93fe67
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-25 20:04:16 -0700

    Added a workaround for a Rails bug.

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -29,3 +29,28 @@ config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2' unless File.
 
 # There is some duplication between test.rb and cucumber.rb.
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => ">=1.2.2", :source => "http://gems.github.com"
+
+# This is a workaround for a Rails bug:
+# https://webrat.lighthouseapp.com/projects/10503/tickets/383-reset_session-and-webrat-dont-play-nicely-with-one-another
+
+unless RAILS_GEM_VERSION == '2.3.8'
+  raise RuntimeError("I'm not sure if this workaround is still necessary")
+end
+
+class RackRailsCookieHeaderHack
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    if headers['Set-Cookie'] && headers['Set-Cookie'].respond_to?(:collect!)
+      headers['Set-Cookie'].collect! { |h| h.strip }
+    end
+    [status, headers, body]
+  end
+end
+
+config.after_initialize do
+  ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, RackRailsCookieHeaderHack)
+end
