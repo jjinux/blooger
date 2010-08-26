5b88ed280b6f725dbaf45f10aae4dc8d4323e59e
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-25 21:03:25 -0700

    Added a spec for Post.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Post do
+  it "can be added to a user" do
+    user = Factory :user
+    user.posts << Post.create!(:title => "Title", :body => "Body")
+    user.save!
+    post = user.posts.first
+    post.title.should == "Title"
+    post.body.should == "Body"
+  end
+end
