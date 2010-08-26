07b9cdc425d04920d5192b2d4c15c4fbfffa17f0
Shannon -jj Behrens <jjinux@gmail.com>
2010-08-26 03:51:25 -0700

    Started setting up the Atom feed.

--- app/controllers/bloogs_controller.rb
+++ app/controllers/bloogs_controller.rb
@@ -6,4 +6,11 @@ class BloogsController < ApplicationController
   def show
     @bloog = User.find(params[:id])
   end
+
+  def atom
+    @bloog = User.find(params[:id])
+    respond_to do |format|
+      format.xml { render :layout => false }
+    end
+  end
 end
