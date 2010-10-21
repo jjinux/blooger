151f1b9a23da443b89fb964fcae0fe840462ccdc
Shannon -jj Behrens <jjinux@gmail.com>
2010-10-20 17:19:26 -0700

    Fixated a version.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,6 +28,6 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
 # There is some duplication between test.rb and cucumber.rb.
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => ">=1.2.2", :source => "http://gems.github.com"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => "1.2.2", :source => "http://gems.github.com"
 config.gem 'rspec',                   :lib => false, :version => '1.3.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails',             :lib => false, :version => '1.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
